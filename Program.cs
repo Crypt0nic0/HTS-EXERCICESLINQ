8ad65aa1f5fa16b1659a7d22ab9be286d10aaed9
Crypt0nic0 <n.greneche45@gmail.com>
2023-04-13 23:07:33 +0200

    finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EXERCICESLINQ;
+
+Console.WriteLine("===========IENUMERABLE===========");
+
+
+var liste = new MaListe(10);
+foreach (var item in liste)
+{
+    Console.WriteLine(item);
+}
+
+
+Console.WriteLine("===========METHODE EXTENSION===========");
+
+int[] tab = new[] { 1, 2, 3, 4, 5 };
+Console.WriteLine(tab.CompterPair());
